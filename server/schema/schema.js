184e859b269457a4dd712c6c4109bad5d41877ac
ayoub3bidi <mrayoubabidi@gmail.com>
2022-07-10 10:46:44 +0100

    ✨ feat: add project & client relationship

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -27,7 +27,13 @@ const ProjectType = new GraphQLObjectType({
         clientId: { type: GraphQLID },
         name: { type: GraphQLString },
         description: { type: GraphQLString },
-        status: { type: GraphQLString }
+        status: { type: GraphQLString },
+        client: { 
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.find(client => client.id === parent.clientId)
+            }
+        }
     })
 })
 
