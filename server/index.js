0d1c36f9798b561e3223be22f5a87881db99a4d6
ayoub3bidi <mrayoubabidi@gmail.com>
2022-07-10 09:48:13 +0100

    ✨ feat: add client type & queries

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema.js')
 require ('dotenv').config();
+
 const port = process.env.PORT || 5000
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
 
 app.listen(port, console.log(`Server running on port ${port}`));
