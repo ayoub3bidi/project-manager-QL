28b22d6ec79acc2e44afec295b9d5427452e886d
ayoub3bidi <mrayoubabidi@gmail.com>
2022-07-11 08:02:44 +0100

    ⚡️ feat: database connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const colors = require('colors')
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema.js')
 require ('dotenv').config();
-
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
 const app = express();
+
+// * Connect to database
+connectDB()
 
 app.use('/graphql', graphqlHTTP({
     schema,
