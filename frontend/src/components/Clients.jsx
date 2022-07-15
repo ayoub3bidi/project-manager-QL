d6dbf65b1a1f93fbaa5fa9711381e5387a6481b5
ayoub3bidi <mrayoubabidi@gmail.com>
2022-07-15 09:45:18 +0100

    ✨ feat: fetch client query

---
+++ frontend/src/components/Clients.jsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@apollo/client"
+import ClientRow from "./ClientRow"
+import { GET_CLIENTS } from "../queries/clientQueries"
+import Spinner from "./Spinner"
+
+const Clients = () => {
+    const { loading, error, data } = useQuery(GET_CLIENTS)
+
+    if (loading) return Spinner
+    if (error) return <p>Something Went Wrong</p>
+  return (
+    <div>
+        {
+            !loading && !error &&
+            ( <table className='table table-hover mt-3'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.clients.map((client) => (
+                        <ClientRow key={client.id} client={client} />
+                    ))}
+                </tbody>
+            
+                </table>)
+        }
+    </div>
+  )
+}
+
+export default Clients
