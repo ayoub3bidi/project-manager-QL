402d3c9cea924e2a469c79159ff81276f8588dac
ayoub3bidi <mrayoubabidi@gmail.com>
2022-07-16 09:14:13 +0100

    ✨ feat: add delete client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,28 @@ import Header from "./components/Header";
 import Clients from "./components/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: { 
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+        projects: { 
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+      }
+    }
+  }
+})
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache: cache,
 })
 
 function App() {
