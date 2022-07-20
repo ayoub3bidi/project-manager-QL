5acd308dbb55c4947ae63cb416bb51c8965f0679
ayoub3bidi <mrayoubabidi@gmail.com>
2022-07-20 09:30:45 +0100

    💄 style: Project style fix

--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -15,16 +15,17 @@ export default function Project() {
     <>
       {!loading && !error && (
         <div className='mx-auto w-75 card p-5'>
-          <Link to='/' className='btn btn-light btn-sm w-25 d-inline ms-auto'>
-            Back
-          </Link>
-
-          <h1>{data.project.name}</h1>
-          <p>{data.project.description}</p>
+          <div className="text-center">            
+            <h1>{data.project.name}</h1>
+            <p>{data.project.description}</p>
+          </div>
           <ClientInfo client={data.project.client} />
 
           <h5 className='mt-3'>Project Status</h5>
           <p className='lead'>{data.project.status}</p>
+          <Link to='/' className='btn btn-dark'>
+            Back
+          </Link>
         </div>
       )}
     </>
